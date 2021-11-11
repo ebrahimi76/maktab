c57a25a7f20d940375b43b9cd4d8ca15a5bac3d8
Mohammad Ali Ebrahimi <44839440+ebrahimi76@users.noreply.github.com>
2021-11-12 01:51:59 +0330

    Update maktab.sql

--- hw13/maktab.sql
+++ hw13/maktab.sql
@@ -4,7 +4,7 @@ CREATE TABLE `course` (
   `name` varchar(100) NOT NULL,
   `unit` int NOT NULL,
   PRIMARY KEY (`c_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3
 CREATE TABLE `course_students` (
   `s_id` int NOT NULL,
   `c_id` int NOT NULL,
@@ -13,12 +13,12 @@ CREATE TABLE `course_students` (
   KEY `course_students_FK_1` (`c_id`),
   CONSTRAINT `course_students_FK` FOREIGN KEY (`s_id`) REFERENCES `student` (`id`),
   CONSTRAINT `course_students_FK_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`)
-) ENGINE=InnoDB DEFAULT CHARSET = utf8mb3
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3
 CREATE TABLE `major` (
   `m_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`m_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET = utf8mb3
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb3
 CREATE TABLE `student` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -27,4 +27,4 @@ CREATE TABLE `student` (
   PRIMARY KEY (`id`),
   KEY `student_FK` (`m_id_fk`),
   CONSTRAINT `student_FK` FOREIGN KEY (`m_id_fk`) REFERENCES `major` (`m_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET = utf8mb3
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb3

95035d59f446ad41f51e5fced687ae5786c80192
EBRAHIMI <ebrahimimohammadali76@gmail.com>
2021-11-16 01:30:21 +0330

    hw14q2update1

--- hw14/hw14q2/src/Main.java
+++ hw14/hw14q2/src/Main.java
@@ -3,9 +3,9 @@ import java.util.function.Predicate;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "iran is big country. we live in iran";
-        Predicate<String> startText = str -> str.startsWith("iran");
-        Predicate<String> endText = str -> str.endsWith("iran");
-        System.out.println(startText.and(endText).test(text));
+        String text = "iran is a big country. we live in iran";
+        Predicate<String> startCheck = str -> str.startsWith("iran");
+        Predicate<String> endCheck = str -> str.endsWith("iran");
+        System.out.println(startCheck.and(endCheck).test(text));
     }
 }

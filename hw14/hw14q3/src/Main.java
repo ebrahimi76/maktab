748f9b52d8bee578083b489b12fe73b5e6b0c3e5
EBRAHIMI <ebrahimimohammadali76@gmail.com>
2021-11-15 23:47:10 +0330

    hw14q3update1

--- hw14/hw14q3/src/Main.java
+++ hw14/hw14q3/src/Main.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        nums.add(3);
-        nums.add(5);
-        nums.add(1);
+        ArrayList<Integer> nums = new ArrayList<>(); // []
+        nums.add(3);                                 // [3]
+        nums.add(5);                                 // [3, 5]
+        nums.add(1);                                 // [3, 5, 1]
         nums.stream()
-                .filter(val -> val > 1)
+                .filter(val -> val > 1)              // [3, 5]
                 .forEach(val -> System.out.println(val));
     }
 }
